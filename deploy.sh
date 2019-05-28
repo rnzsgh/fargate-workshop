d70e4b60694e7f44cb0fb95b531847196114dbac
Ryan <ryannitz@amazon.com>
2019-05-28 18:43:08 -0500

    Initial

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BUCKET_NAME=public-fargate-intro-workshop
+
+aws s3 sync public/ s3://$BUCKET_NAME/
+
+aws s3api list-objects --bucket $BUCKET_NAME --query 'Contents[].{Key:Key}' --output text | xargs -n 1 aws s3api put-object-tagging --bucket $BUCKET_NAME --tagging 'TagSet=[{Key=public,Value=yes}]' --key
+
+
+
+
