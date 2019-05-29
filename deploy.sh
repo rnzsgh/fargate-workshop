099d25b38f701845ff7da4f8e10dc9b3ef8d418a
Ryan <ryannitz@amazon.com>
2019-05-29 07:58:26 -0500

    Add build call

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 BUCKET_NAME=public-fargate-intro-workshop
 
+npm run build
+
 aws s3 sync public/ s3://$BUCKET_NAME/
 
 aws s3api list-objects --bucket $BUCKET_NAME --query 'Contents[].{Key:Key}' --output text | xargs -n 1 aws s3api put-object-tagging --bucket $BUCKET_NAME --tagging 'TagSet=[{Key=public,Value=yes}]' --key
